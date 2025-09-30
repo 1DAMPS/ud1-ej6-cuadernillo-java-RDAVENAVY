70f7f95bae3eeaa14745d61b097f87b75537b2d4
RDAVENAVY <david.navarro.reyes.al@iespoligonosur.org>
2025-09-30 17:09:39 +0000

    subida ej06

---
+++ ej06.java
@@ -0,0 +1,9 @@
+public class ej06{
+    public static void main(String[] args){
+        int numeroPrimo=3;
+        char nombreCompleto= ´Lola Lalo´;
+        double numeroImpar=3.1;
+
+        System.out.println("Mostrar: "+ numeroPrimo+", "+nombreCompleto+" y "+ numeroImpar);
+    }
+}
