024dc4c0a7539a0a692f5165ce03800fbd37e2db
RDAVENAVY <david.navarro.reyes.al@iespoligonosur.org>
2025-09-30 17:13:58 +0000

    subida ej02, ej3, ej4 , ej5, ej6, ej7

---
+++ ej02.java
@@ -0,0 +1,8 @@
+public class ej02{
+    public static void main(String[] args){
+        
+        System.out.println("David");
+        System.out.println("22");
+
+    }
+}
--- ej06.java
+++ ej06.java
@@ -1,7 +1,7 @@
 public class ej06{
     public static void main(String[] args){
         int numeroPrimo=3;
-        char nombreCompleto= ´Lola Lalo´;
+        char nombreCompleto= 'L';
         double numeroImpar=3.1;
 
         System.out.println("Mostrar: "+ numeroPrimo+", "+nombreCompleto+" y "+ numeroImpar);
